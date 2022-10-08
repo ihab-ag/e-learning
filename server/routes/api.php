eec5fac66c407b5ea4bac0effa505fe1bd2f4fad
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 20:55:39 +0300

    backend <> feat(api): assign instructor route

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 
-
-Route::post('test',[AdminController::class, 'assignInstructor']);
 Route::post('login',[AuthController::class, 'login']);
 
 Route::group(["middleware" => "auth"], function(){
     Route::get('refresh',[AuthController::class, 'refresh']);
     Route::post('add-user',[AdminController::class,'addUser']);
     Route::get('add-course',[AdminController::class,'addCourse']);
+    Route::post('asign-instructor',[AdminController::class, 'assignInstructor']);
 });
     
