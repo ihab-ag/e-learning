7b0bb93d11e861ff8c06b8378fafb6b4f8f91669
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 19:32:34 +0300

    backend <> feat(AdminController): add user

---
+++ server/app/Http/Controllers/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    
+    function addUser(Request $request){
+       
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password'=>[
+                'required',
+                'min:6',
+                'regex:/[a-z]/',
+                'regex:/[0-9]/',
+                'regex:/[A-Z]/'
+            ],
+            'type'=>'required'
+        ]);
+        
+        $user= new User;
+
+        $user->name = $request -> name;
+        $user-> email = $request -> input("email");
+        $user-> password = Hash::make($request -> password);
+        $user-> type = $request -> type;
+        
+        $type= $request->type;
+        
+        if($type=='student'){
+            $user->courses= "";
+        }
+
+        $user->save();
+
+        return response()->json([
+            "message"=>"Success",
+            200
+        ]);
+    }
+}
