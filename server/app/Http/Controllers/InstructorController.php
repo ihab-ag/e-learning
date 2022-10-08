a4b21e54c4d5c9ab5902cc721fcbbb74b8d2a52e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 22:26:50 +0300

    backend <> feat(InstructorController): get courses

--- server/app/Http/Controllers/InstructorController.php
+++ server/app/Http/Controllers/InstructorController.php
@@ -10,5 +10,21 @@ use App\Models\Course;
 
 class InstructorController extends Controller
 {
-    //
+    public function getCourses(){
+
+        // $instructor= Auth::user();
+        // $instructor_id= $instructor->id;
+
+        $response=[];
+
+        $courses= Course::where('instructor', $instructor_id)->get();
+        foreach($courses as $course){
+            $response[]=$course;
+        }
+
+        return response()->json([
+            "status"=>200,
+            "courses"=>$response
+        ]);
+    }
 }
