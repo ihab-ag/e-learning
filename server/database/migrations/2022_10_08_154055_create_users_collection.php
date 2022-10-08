46472207959d5de3705a5f21daac39acc9b77064
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 18:47:52 +0300

    backend<> config: migrations

--- server/database/migrations/2022_10_08_154055_create_users_collection.php
+++ server/database/migrations/2022_10_08_154055_create_users_collection.php
@@ -13,14 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('users', function (Blueprint $table) {
+
+        });
+        Schema::create('courses', function (Blueprint $table) {
+
+        });
+        Schema::create('assignments', function (Blueprint $table) {
+
+        });
+        Schema::create('announcements', function (Blueprint $table) {
+
         });
     }
 
@@ -31,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('users_collection');
     }
 };
