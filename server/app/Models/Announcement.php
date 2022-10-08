a6a0a1b66cc433a6f8709e29ee97743bd3f57d5a
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 23:37:52 +0300

    backend<> fix: announcement typo

--- server/app/Models/Announcement.php
+++ server/app/Models/Announcement.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class Annoncement extends Model
+class Announcement extends Model
 {
     use HasFactory;
 
