86a36828877ceab6157386d1ccfa5c68d06a8da5
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 22:48:54 +0300

    backend <> config: Assignment model

---
+++ server/app/Models/Assignment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Assignment extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'course',
+        'submission'
+    ];
+
+    protected $attributes = [
+        'submissions' => [],
+    ];
+}
