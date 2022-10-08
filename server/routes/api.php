5583ad6c155fd88ccd5a955dda73a1722dc9f3fd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 20:25:47 +0300

    backend <> feat(api): add course route

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,5 +12,6 @@ Route::post('login',[AuthController::class, 'login']);
 Route::group(["middleware" => "auth"], function(){
     Route::get('refresh',[AuthController::class, 'refresh']);
     Route::post('add-user',[AdminController::class,'addUser']);
+    Route::get('add-course',[AdminController::class,'addCourse']);
 });
     
