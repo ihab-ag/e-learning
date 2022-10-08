33e197cfcc5436a0a67921de7ad2aad058d60102
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 20:04:40 +0300

    backend <> feat(api): add user route

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,11 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 
-Route::post('add',[AdminController::class,'addUser']);
+
 
 Route::post('login',[AuthController::class, 'login']);
 
 Route::group(["middleware" => "auth"], function(){
     Route::get('refresh',[AuthController::class, 'refresh']);
+    Route::post('add-user',[AdminController::class,'addUser']);
 });
     
