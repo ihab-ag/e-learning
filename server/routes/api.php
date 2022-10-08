327cd94fe68cbc1c10c1d5eab25b5b8e59eedbf4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 21:06:15 +0300

    backend <> feat(api): admin middleware

--- server/routes/api.php
+++ server/routes/api.php
@@ -8,9 +8,14 @@ use App\Http\Controllers\AdminController;
 Route::post('login',[AuthController::class, 'login']);
 
 Route::group(["middleware" => "auth"], function(){
+
     Route::get('refresh',[AuthController::class, 'refresh']);
-    Route::post('add-user',[AdminController::class,'addUser']);
-    Route::get('add-course',[AdminController::class,'addCourse']);
-    Route::post('asign-instructor',[AdminController::class, 'assignInstructor']);
+
+    Route::group(["middleware"=> "admin"],function(){
+        Route::post('add-user',[AdminController::class,'addUser']);
+        Route::get('add-course',[AdminController::class,'addCourse']);
+        Route::post('asign-instructor',[AdminController::class, 'assignInstructor']);
+    });
+    
 });
     
