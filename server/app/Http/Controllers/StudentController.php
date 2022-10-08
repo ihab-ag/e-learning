3903a238ef1b789c19501a3a8ccff97099d75dba
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-09 00:13:12 +0300

    backend<> config: StudentController

---
+++ server/app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\Assignment;
+use App\Models\Announcement;
+
+class StudentController extends Controller
+{
+    //
+}
