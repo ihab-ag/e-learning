b0f245873c6050170a6b3e6fbd49ce2dcd4a1be9
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 19:01:46 +0300

    backend <> feat(AuthController): login

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Unauthorized',
+            ]);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+                'status' => 200,
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+    }
+}
