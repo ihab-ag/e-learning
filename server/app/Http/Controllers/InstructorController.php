6f3cd47f06c233586a3dba4d6a172dfb2dd2a2bd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 22:04:44 +0300

    backend <> config: InstructorController

---
+++ server/app/Http/Controllers/InstructorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Course;
+
+class InstructorController extends Controller
+{
+    //
+}
