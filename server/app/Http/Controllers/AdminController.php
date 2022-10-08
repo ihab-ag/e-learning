67ff83bb433ed0784f1b1d70f4ef48b61b7db2fb
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-08 21:59:02 +0300

    backend <> fix(AdminController): student courses

--- server/app/Http/Controllers/AdminController.php
+++ server/app/Http/Controllers/AdminController.php
@@ -35,7 +35,7 @@ class AdminController extends Controller
         $type= $request->type;
         
         if($type=='student'){
-            $user->courses= "";
+            $user->courses= [];
         }
 
         $user->save();
