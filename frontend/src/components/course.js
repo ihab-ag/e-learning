b66ec6dabba394d662bdeb0b0dc657b318d832af
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-10 23:26:26 +0300

    frontend<> feat: courses

---
+++ frontend/src/components/course.js
@@ -0,0 +1,11 @@
+const Course=({name})=>{
+    return(
+        <>
+            <div className="course">
+                <p>{name}</p>
+            </div>
+        </>
+    )
+}
+
+export default Course;
