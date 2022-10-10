da733a01f08e51aaf7614ee6824d62f86c12851a
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-10 23:26:43 +0300

    frontend<> feat: assignments

--- frontend/src/components/page.js
+++ frontend/src/components/page.js
@@ -1,5 +1,7 @@
 import AnnouncementRegister from "./announcement_register";
+import Assignment from "./assignments";
 import AssignmentRegister from "./assignment_register";
+import Course from "./course";
 import CourseAssigner from "./course_assigner";
 import CourseRegister from "./course_register";
 import UserRegister from "./user_register";
@@ -41,6 +43,19 @@ const Page=({page})=>{
             {
                 page === 'announcement' && (
                     <AnnouncementRegister />
+                )
+            }
+            {
+                page === 'courses' && (
+                    <> <Course
+                   name="Course" />
+                    <Course
+                   name="Course" /></>
+                )
+            }
+            {
+                page === 'assignments' && (
+                    <Assignment />
                 )
             }
         </div>
