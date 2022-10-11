deff0dedcd2fc6813666d1a73cc93eab3fe8daef
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-11 21:03:48 +0300

    frontend<> feat: assign instructor to course

--- frontend/src/components/course_assigner.js
+++ frontend/src/components/course_assigner.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import { assignInstructor } from "../hooks/assign_instructor";
 
 const CourseAssigner= ({name,select,id})=>{
     const [instructor,setInstructor] = useState('');
     const instructors = select.data.instructors;
+    const sendReq=(e)=>{
+        e.preventDefault()
+        const data = new FormData();
+        data.append('instructor',instructor)
+        data.append('id',id);
+        assignInstructor(data);
+    }
 
 return(
     <>
-        <form className="form course flex-row" onSubmit={(e)=> e.preventDefault()}>
+        <form className="form course flex-row" onSubmit={(e)=> sendReq(e) }>
             <p>{name}</p>
             <div className="flex-row course__div">
                 <select name="id" className="select" onChange={(e)=>setInstructor(e.target.value)}>
