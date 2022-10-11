77bfd7950b7ed7d5f294f89395ba39efede41a57
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-11 23:45:35 +0300

    frontend<> feat: add announcement

--- frontend/src/components/announcement_register.js
+++ frontend/src/components/announcement_register.js
@@ -1,12 +1,20 @@
 import { useState } from "react"
+import AssignmentRegister from "./assignment_register";
 
 const AnnouncementRegister =()=>{
     const [name,setName]= useState('');
     const [text,setText]= useState('');
+    const sendReq=(e)=>{
+        e.preventDefault()
+        const data = new FormData();
+        data.append('name',name)
+        data.append('text',text);
+        AssignmentRegister(data);
+    }
     return(
         <>
             <h1>Add Announcement</h1>
-            <form className="form flex-column" onSubmit={(e)=> e.preventDefault()}>
+            <form className="form flex-column" onSubmit={(e)=> sendReq(e)}>
                 <p>Name:</p>
                 <input type="text" name="name" value={name} onChange={(e)=> setName(e.target.value)} className="input__text" />
                 <p>Info text:</p>
